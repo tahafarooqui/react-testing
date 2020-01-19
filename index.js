153b9061e3a3234a114bc6e3e0e9fd2c7fe89625
tahafarooqui <tahafarooquitech@gmail.com>
2020-01-20 02:01:46 +0500

    1 file modified

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ var firebase = require("firebase/app");
 // Add the Firebase products that you want to use
 require("firebase/auth");
 require("firebase/firestore");
+      var db = firebase.firestore();
 
   var firebaseConfig = {
     apiKey: "AIzaSyDDU8PiSVhHT8MOPuC_RbXHuJnYIDASrwg",
@@ -20,7 +21,9 @@ require("firebase/firestore");
     measurementId: "G-5D2KRRV1ZQ"
   };
   // Initialize Firebase
+  if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
+  }
 
 class App extends Component {
   constructor() {
@@ -30,7 +33,80 @@ class App extends Component {
     };
   }
 
+
+  signup(){
+        firebase.auth().createUserWithEmailAndPassword('tahafarooquitech@gmail.com', 'taha12345').catch(function(error) {
+  // Handle Errors here.
+  console.log(error.code);
+  console.log(error.message);
+});
+  }
+
+
+  signin(){
+    firebase.auth().signInWithEmailAndPassword('tahafarooquitech@gmail.com', 'taha12345').catch(function(error) {
+  // Handle Errors here.
+    console.log(error.code);
+  console.log(error.message);
+  // ...
+});
+  }
+
+  signOut(){
+    firebase.auth().signOut().then(function() {
+  // Sign-out successful.
+}).catch(function(error) {
+  // An error happened.
+});
+  }
+
+  getprofile(){
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    // User is signed in.
+    var displayName = user.displayName;
+    var email = user.email;
+    var emailVerified = user.emailVerified;
+    var photoURL = user.photoURL;
+    var isAnonymous = user.isAnonymous;
+    var uid = user.uid;
+    var providerData = user.providerData;
+
+    console.log(email);
+
+  } else {
+    // User is signed out.
+    // ...
+  }
+});
+  }
+
+  insertData(){
+      db.collection("users").add({
+          first: "Ada",
+          last: "Lovelace",
+          born: 1815
+      })
+      .then(function(docRef) {
+          console.log("Document written with ID: ", docRef.id);
+      })
+      .catch(function(error) {
+          console.error("Error adding document: ", error);
+      });
+
+  }
+
   render() {
+
+  var docRef = db.collection('users').doc('CN48DNRwqhZpqkpWMq1Y');
+docRef.update({
+    name: "Frankdd",
+
+ });
+
+this.getprofile();
+
+
     return (
       <div>
         <Hello name={this.state.name} />
